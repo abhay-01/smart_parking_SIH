23b4bd58477684b89dc0b8e7fdf50c2671a5b09d
abhay-01 <pratapsinghabhay999@gmail.com>
2023-09-12 22:47:13 +0530

    Maps

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -4,34 +4,35 @@ import OnboardingScreen from '../screens/Onboarding';
 import HomeScreen from '../screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState } from 'react';
+import { getData } from '../utils/AsyncStrorage';
 
 const  Stack = createStackNavigator();
 export default function AppNavigation() {
-    const[showOnboard, setOnboard] = useState(null);
+    // const[showOnboard, setOnboard] = useState(null);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        checkSignUp();
-    }, []);
+    //     checkSignUp();
+    // }, []);
 
-    const checkSignUp = async () => {
-        const value = await getData('signedup');
-        if(value === 1){
-            //hide onboarding screen
-            showOnboard(false);
-        }else{
+    // const checkSignUp = async () => {
+    //     const value = await getData('signedup');
+    //     if(value === 1){
+    //         //hide onboarding screen
+    //         showOnboard(false);
+    //     }else{
 
-            //show onboarding screen
-            showOnboard(true);
-        }
-    }
+    //         //show onboarding screen
+    //         showOnboard(true);
+    //     }
+    // }
 
-    if(showOnboard == null){
-        return null;
-    }
+    // if(showOnboard == null){
+    //     return null;
+    // }
   
 
-    if(showOnboard){
+    // if(showOnboard){
         return(
             <NavigationContainer>
         <Stack.Navigator initialRouteName='Onboarding'>
@@ -44,20 +45,16 @@ export default function AppNavigation() {
         </Stack.Navigator>
     </NavigationContainer>
         )
-    }else{
-        <NavigationContainer>
-        <Stack.Navigator initialRouteName='HomeScreen'>
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} options = {{
-                headerShown: false
-            }}/>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
-                headerShown: false
-            }}/>
-        </Stack.Navigator>
-    </NavigationContainer>
-    }
-
-    
-   
-  )
+    // }else{
+    //     <NavigationContainer>
+    //     <Stack.Navigator initialRouteName='HomeScreen'>
+    //         <Stack.Screen name="Onboarding" component={OnboardingScreen} options = {{
+    //             headerShown: false
+    //         }}/>
+    //         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+    //             headerShown: false
+    //         }}/>
+    //     </Stack.Navigator>
+    // </NavigationContainer>
+    // }
 }
